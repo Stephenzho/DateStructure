68700aaaa9efcf98d4361329d05dc19feb6da7aa
Stephenzho <stephen_zho@126.com>
2018-11-07 22:21:07 +0800

    三路快速排序

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -1,7 +1,5 @@
 package sort;
 
-import java.util.Random;
-
 /**
  * 快速排序
  * @author zhoushuyi
@@ -10,8 +8,44 @@ import java.util.Random;
 public class QuickSort {
 
     public static void sort(int[] arr) {
-        quickSort(arr, 0, arr.length - 1);
+        quickSort3(arr, 0, arr.length - 1);
     }
+
+
+    /**
+     * 三路快速排序
+     * @param arr
+     * @param l
+     * @param r
+     */
+    private static void quickSort3(int[] arr, int l, int r) {
+        swap(arr, l, (int) Math.random() % (r - l + 1) + l);
+
+        int v = arr[l];
+
+        int lt = l;
+        int gt = r + 1;
+        int e = l + 1;
+
+        while (gt > e) {
+            if (arr[e] > v) {
+                swap(arr, gt - 1, e);
+                gt--;
+            } else if (arr[e] < v) {
+                swap(arr, lt + 1, e);
+                lt++;
+                e++;
+            } else {
+                e++;
+            }
+        }
+        swap(arr, lt, l);
+
+        quickSort(arr, l, lt - 1);
+        quickSort(arr, gt, r);
+    }
+
+
 
     private static void quickSort(int[] arr, int l, int r) {
 
@@ -19,7 +53,7 @@ public class QuickSort {
             return;
         }
 
-        int k = partition(arr, l, r);
+        int k = partition2(arr, l, r);
 
         quickSort(arr, l, k - 1);
         quickSort(arr, k + 1, r);
@@ -40,6 +74,34 @@ public class QuickSort {
         swap(arr, j, l);
         return j;
     }
+
+    private static int partition2(int[] arr, int l, int r) {
+        swap(arr, l, (int) Math.random() % (r - l + 1) + l);
+
+        int v = arr[l];
+        int i = l+1, j = r;
+
+        while (true) {
+            while (i <= r && arr[i] < v) {
+                i++;
+            }
+            while (j >= l+1 && arr[j] > v) {
+                j--;
+            }
+
+            if (i > j) {
+                break;
+            }
+            swap(arr, i, j);
+            i++;
+            j--;
+        }
+        swap(arr, l, j);
+
+        return j;
+    }
+
+
 
     private static void swap(int[] arr, int i, int j) {
         int n = arr[i];
